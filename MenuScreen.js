e0a7a8fa2d2befa4c810df679c57e197dc0091d5
jvsobraz <jvsobraz@hotmail.com>
2024-01-16 21:48:57 -0300

    Tela Menu

---
+++ MenuScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function MenuScreen() {
+  return (
+    <View>
+      <Text>Menu Screen</Text>
+      {/* Add menu items and functionality here */}
+    </View>
+  );
+}
+
+export default MenuScreen;
