b89c7c30cc4423b9436ffb7ab0c733ded99b2085
jvsobraz <jvsobraz@hotmail.com>
2024-01-16 21:52:24 -0300

    Introduzindo a API

--- MenuScreen.js
+++ MenuScreen.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 function MenuScreen() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.sampleapis.com/coffee/hot')
+      .then(response => setMenuItems(response.data))
+      .catch(error => console.error('Error fetching menu:', error));
+  }, []);
+
   return (
     <View>
       <Text>Menu Screen</Text>
-      {/* Add menu items and functionality here */}
+      {menuItems.map(item => (
+        <Text key={item.id}>{item.name} - ${item.price}</Text>
+      ))}
     </View>
   );
 }
